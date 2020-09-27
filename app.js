7a45c774438eb0ebc675b74cbf9fb097717b1ea2
Eric D. Torres <etorresnotary@gmail.com>
2020-09-26 21:41:01 -0500

    updating validation object file with capital and removing validate e-mail from app file

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const Validate = require("./lib/Validation");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const validEMail = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
