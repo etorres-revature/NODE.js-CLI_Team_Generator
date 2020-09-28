55f995eece9543262d7b64f2eb8566af439a7b9c
Eric D. Torres <etorresnotary@gmail.com>
2020-09-27 19:27:37 -0500

    adding comments to Validation.test.js

--- test/validation.test.js
+++ test/validation.test.js
@@ -1,6 +1,9 @@
+//uses require funciton to bring in Validation 
 const Validate = require("../lib/Validation");
 
+//test for validateNull function
 describe("validateNull", () => {
+  //tests for validation function for when an empty string is entered into prompt
   it("validates for a null value entry", () => {
     const v = new Validate();
     const methodReturn = `You MUST enter something to continue.`;
@@ -10,7 +13,9 @@ describe("validateNull", () => {
   });
 });
 
+//test function for validateNullAndNumber
 describe("validateNullANumber", () => {
+  //texts for validation function for when an empty string is entered into prompt
   it("validates for a null value entry", () => {
     const v = new Validate();
     const methodReturn = `You MUST enter something to continue.`;
@@ -19,6 +24,7 @@ describe("validateNullANumber", () => {
     expect(result).toEqual(methodReturn);
   });
 
+  //tests for validation function for when a letter is entered into the prompt
   it("validates that the entry is a number", () => {
     const v = new Validate();
     const methodReturn = `You MUST enter a whole number value.`;
@@ -28,7 +34,9 @@ describe("validateNullANumber", () => {
   });
 });
 
+//text function for validateEmail
 describe("validateEMail", () => {
+//tests for what happens when anything not matching an expected value for an e-mail
   it("validates that an E-mail with '@' and '.com' were entered", () => {
     const v = new Validate();
     const methodReturn = `You MUST enter a valid e-mail address including the "@" symbol and a top-level domain (TLD; such as, ".com"; ".net"; ".edu"; etc.)`;
@@ -38,7 +46,9 @@ describe("validateEMail", () => {
   });
 });
 
+//text function for validateConfirm
 describe("validateConfirm", () => {
+  //tests for what happens when something other than the letter "y" or the letter "n" is entered
   it("detects whether it is 'y' or 'n' that was entered", () => {
     const v = new Validate();
     const methodReturn = `Please input a 'y' or 'n' to continue`;
